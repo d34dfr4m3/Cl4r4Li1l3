67b16aae2e82c89e30d954bac9f45d695af4f802
d34dfr4m3 <32407502+d34dfr4m3@users.noreply.github.com>
2019-02-02 15:30:32 -0200

    Update claralille.py

--- claralille.py
+++ claralille.py
@@ -1,42 +1,40 @@
 #! /usr/bin/python3
 from telethon import TelegramClient, sync, utils
+
 from telethon.tl.types import PeerUser
 from classbot import Bot
 from time import sleep
-api_id = #
-api_hash = #
-acervo = #
-target_handler = #
 
-exts_perm = [".pdf",".zip",".epub",'.doc','.txt'] #extensoes que serao redirecionadas
-
+api_id = INTEGER
+api_hash = 'STRING'
+acervo = "STRING"
+# Extensões aceitas para serem encaminhadas.
+exts_perm = [".pdf",".zip",".epub",'.doc','.txt']
+group='SOME_STRING_ORWHATEVER_ENTITY_STUFF'
 
 client = TelegramClient('clara_memory', api_id, api_hash)
 client.start()
-target_handler = client.get_entity('https://t.me/joinchat/IA-by07Fa_k4J7Y1JB0JIQ')
+target_handler = client.get_entity(group)
 client = Bot(client)
 print("[*] Running...")
-oldID=None
+oldID = None
 client.sendMsg(target_handler,"Hi, Clara Lille here!")
 try:
     while 1:
-        sleep(0.6) # o sleep evita sobrecarga. Evitando Travamentos demorados
+        sleep(0.6) # o sleep evita sobrecarga. Evitando Travamentos demorados n lembro porque isso ta aq 
         messagesblock,oldID = client.getMsg(target_handler,oldID)
         if messagesblock:
-            for i in messagesblock:
-                if i.media: #verifica se a msg eh um arquivo.
-                    file = i.media
-                    ext = utils.get_extension(file)
-                    if ext in exts_perm:
-                        filename = i.media.document.attributes[0].file_name
+            for msg in messagesblock:
+                if msg.media:
+                    if utils.get_extension(msg.media) in exts_perm:
+                        filename = msg.media.document.attributes[0].file_name
                         if client.searchmedia(acervo,filename):
-                            client.forwardfile(i, acervo)
-                            client.msg_reply(i,"[*] Upload Sucessfull to @Ac3rv0b4t4t4, by Clara Lille",target_handler)
+                            client.forwardfile(msg, acervo)
+                            client.msg_reply(msg,"[*] Upload Sucessfull to "+ acervo +", by Clara Lille",target_handler)
                         else:
-                            client.msg_reply(i,"[*] File Exist, by Clara Lille", target_handler)
-                    # Se não for uma media, verifica se é um comando: 
+                            client.msg_reply(msg,"[*] File Exist, by Clara Lille", target_handler)
                 else:
-                        client.check_command(i,target_handler,acervo)
+                        client.check_command(msg,target_handler,acervo)
 except KeyboardInterrupt:
     client.sendMsg(target_handler, "i'll AFK, bye")
     client.dead()
